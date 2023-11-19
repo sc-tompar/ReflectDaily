6cc5f3c1731d2c9c8bf77c35482ac2c1cfdc8c73
JerryDoriquez <143570720+JerryDoriquez@users.noreply.github.com>
2023-11-19 15:08:37 +0800

    ReflectionMenu.js

---
+++ ReflectionMenu.js
@@ -0,0 +1,177 @@
+import React from "react";
+import "./ReflectionMenu.css";
+
+export const ReflectionMenu = () => {
+  return (
+    <div className="reflection-menu">
+      <div className="div">
+        <div className="overlap">
+          <img className="menu-icon" alt="Menu icon" src="menu-icon.svg" />
+          <img className="line" alt="Line" src="line-1.svg" />
+          <img className="qoutes-button" alt="Qoutes button" src="qoutes-button.png" />
+          <div className="overlap-group">
+            <img className="reflection-button" alt="Reflection button" src="reflection-button.png" />
+            <div className="highlight-effects" />
+          </div>
+          <img className="streak-button" alt="Streak button" src="streak-button.png" />
+          <img className="sign-out-button" alt="Sign out button" src="sign-out-button.png" />
+          <div className="avatar-icon">
+            <div className="status-online" />
+          </div>
+        </div>
+        <div className="overlap-2">
+          <img className="avatar" alt="Avatar" src="avatar-2.png" />
+          <div className="div-wrapper">
+            <div className="text-wrapper">Type to search...</div>
+          </div>
+          <img className="vector" alt="Vector" src="vector-15.svg" />
+          <p className="reflect-daily">
+            <span className="span">Reflect</span>
+            <span className="text-wrapper-2">Daily</span>
+          </p>
+        </div>
+        <div className="text-wrapper-3">Reflection</div>
+        <div className="overlap-3">
+          <div className="text-wrapper-4">Mood</div>
+          <img className="more-icon" alt="More icon" src="more-icon.svg" />
+        </div>
+        <div className="overlap-4">
+          <div className="text-wrapper-4">Tags</div>
+          <img className="more-icon" alt="More icon" src="image.svg" />
+        </div>
+        <div className="overlap-5">
+          <div className="text-wrapper-5">Latest</div>
+          <img className="img" alt="More icon" src="more-icon-2.svg" />
+        </div>
+        <div className="vector-wrapper">
+          <img className="vector-2" alt="Vector" src="vector-2.svg" />
+        </div>
+        <div className="img-wrapper">
+          <img className="vector-3" alt="Vector" src="vector.svg" />
+        </div>
+        <div className="overlap-6">
+          <div className="rereflection-notes">
+            <div className="overlap-group-2">
+              <div className="overlap-7">
+                <div className="overlap-group-3">
+                  <p className="p">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley o
+                  </p>
+                  <div className="text-wrapper-6">HAPPY</div>
+                  <img className="line-2" alt="Line" src="line-3.svg" />
+                </div>
+                <div className="text-wrapper-7">November 18, 2023</div>
+              </div>
+              <div className="text-wrapper-8">CRUSH</div>
+              <img className="vector-4" alt="Vector" src="vector-3.svg" />
+              <img className="vector-5" alt="Vector" src="vector-4.svg" />
+            </div>
+          </div>
+          <img className="vector-6" alt="Vector" src="vector-16.svg" />
+        </div>
+        <div className="group">
+          <div className="overlap-group-2">
+            <div className="overlap-7">
+              <div className="overlap-group-3">
+                <p className="p">
+                  Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
+                  classical Latin literature from 45 BC, making it over 2000 years old.
+                </p>
+                <div className="text-wrapper-9">MAD</div>
+                <img className="line-2" alt="Line" src="line-6.svg" />
+              </div>
+              <div className="text-wrapper-10">November 19, 2023</div>
+            </div>
+            <div className="text-wrapper-11">Frenemy</div>
+            <img className="vector-4" alt="Vector" src="vector-7.svg" />
+            <img className="vector-5" alt="Vector" src="vector-11.svg" />
+            <img className="vector-7" alt="Vector" src="vector-17.svg" />
+          </div>
+        </div>
+        <div className="overlap-wrapper">
+          <div className="overlap-group-2">
+            <div className="overlap-7">
+              <div className="overlap-group-3">
+                <p className="p">
+                  There are many variations of passages of Lorem Ipsum available, but the majority have suffered
+                  alteration in some form, by injected humour, or randomised words which don&#39;t look even slightly
+                  believable.
+                </p>
+                <div className="text-wrapper-12">CURIOUS</div>
+                <img className="line-2" alt="Line" src="line-8.svg" />
+              </div>
+              <div className="text-wrapper-13">November 20, 2023</div>
+            </div>
+            <div className="text-wrapper-14">Chika</div>
+            <img className="vector-4" alt="Vector" src="vector-9.svg" />
+            <img className="vector-5" alt="Vector" src="vector-13.svg" />
+            <img className="vector-8" alt="Vector" src="vector-18.svg" />
+          </div>
+        </div>
+        <div className="overlap-group-wrapper">
+          <div className="overlap-group-2">
+            <div className="overlap-7">
+              <div className="overlap-group-3">
+                <p className="p">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                </p>
+                <div className="text-wrapper-9">MAD</div>
+                <img className="line-2" alt="Line" src="line-7.svg" />
+              </div>
+              <div className="text-wrapper-13">November 30, 2023</div>
+            </div>
+            <div className="text-wrapper-11">Frenemy</div>
+            <img className="vector-4" alt="Vector" src="vector-8.svg" />
+            <img className="vector-5" alt="Vector" src="vector-12.svg" />
+            <img className="vector-8" alt="Vector" src="vector-19.svg" />
+          </div>
+        </div>
+        <div className="group-2">
+          <div className="overlap-8">
+            <div className="rereflection-notes">
+              <div className="overlap-group-2">
+                <div className="overlap-7">
+                  <div className="overlap-group-3">
+                    <p className="p">
+                      It is a long established fact that a reader will be distracted by the readable content of a page
+                      when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+                      distribution
+                    </p>
+                    <div className="text-wrapper-6">HAPPY</div>
+                    <img className="line-2" alt="Line" src="line-3-2.svg" />
+                  </div>
+                  <div className="text-wrapper-15">November 29, 2023</div>
+                </div>
+                <div className="text-wrapper-8">CRUSH</div>
+                <img className="vector-4" alt="Vector" src="vector-5.svg" />
+                <img className="vector-5" alt="Vector" src="vector-6.svg" />
+              </div>
+            </div>
+            <img className="vector-9" alt="Vector" src="vector-20.svg" />
+          </div>
+        </div>
+        <div className="group-3">
+          <div className="overlap-group-2">
+            <div className="overlap-7">
+              <div className="overlap-group-3">
+                <p className="p">
+                  &#34;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                  labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                  ut aliquip ex ea commodo conse...
+                </p>
+                <div className="text-wrapper-12">CURIOUS</div>
+                <img className="line-2" alt="Line" src="line-9.svg" />
+              </div>
+              <div className="text-wrapper-16">November 21, 2023</div>
+            </div>
+            <div className="text-wrapper-14">Chika</div>
+            <img className="vector-4" alt="Vector" src="vector-10.svg" />
+            <img className="vector-5" alt="Vector" src="vector-14.svg" />
+            <img className="vector-8" alt="Vector" src="vector-21.svg" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
