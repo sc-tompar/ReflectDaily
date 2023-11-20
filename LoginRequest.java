3f207b10a6e3e6963b907af5db50ec77474e12c6
Wrecage <121277075+Wrecage@users.noreply.github.com>
2023-11-21 00:40:40 +0800

    LoginRequest.java

--- LoginRequest.java
+++ LoginRequest.java
@@ -1,1 +1,25 @@
 
+package com.csit321.Espanot;
+
+public class LoginRequest {
+    private String email;
+    private String password;
+
+    // Getters and Setters
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
