e714e3ac14fff942375b23039d3b29dbd1cf906f
Wrecage <121277075+Wrecage@users.noreply.github.com>
2023-11-22 22:36:43 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
-import Login from './Login';
-import Register from './Register';
-import Home from './Home'; 
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import AboutUs from './pages/AboutUs'; 
+import ContactUs from './pages/ContactUs'; 
+import RegSignLanding from './pages/regsignlanding';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Sidebar from './components/Sidebar';
+import Navbar from './components/Navbar'; // Import the Navbar component
+import Dashboard from './pages/Dashboard.jsx';
+import Quotes from './pages/Quotes.jsx';
+import Reflection from './pages/Reflection.jsx';
+import Streak from './pages/Streak.jsx';
+ 
 
-function App() {
-  return (
-    <Router>
-      <Routes> 
-        <Route exact path="/" element={<Home />} /> 
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
-  );
-}
+const App = () => {
 
+return (
+<Router>
+ <Routes>
+  {/* Routes for main content */}
+  <Route path="/" element={<LandingPage />} />
+  <Route path="/about" element={<AboutUs />} />
+  <Route path="/contact" element={<ContactUs />} />
+  <Route path="/regsignlanding" element={<RegSignLanding />} />
+  <Route path="/login" element={<Login />} />
+  <Route path="/register" element={<Register />} />
+
+  {/* Nested route for dashboard content */}
+  <Route
+          path="/dashboard/*"
+          element={
+            <div className="app-container">
+              <Sidebar />
+              <div className="main-container">
+                <Navbar />
+                <Routes>
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="/quotes" element={<Quotes />} />
+                  <Route path="/reflection" element={<Reflection />} />
+                  <Route path="/streak" element={<Streak />} />
+                </Routes>
+              </div>
+            </div>
+          }
+        />
+</Routes>
+</Router>
+);
+};
+ 
 export default App;
+ 
