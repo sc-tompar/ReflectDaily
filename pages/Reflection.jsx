cac9a1cd132c721b5c791a906707c6615abd990c
Wrecage <121277075+Wrecage@users.noreply.github.com>
2023-12-06 02:07:55 +0800

    Update Reflection.jsx

--- pages/Reflection.jsx
+++ pages/Reflection.jsx
@@ -1,54 +1,80 @@
-import React from 'react';
-import { useState } from 'react';
-import { nanoid } from 'nanoid';
+import React, { useState, useEffect } from 'react';
 import '../styles/Reflection.css';
 import NotesList from '../components/NotesList';
 import Search from '../components/Search';
-//import AddNote from '../components/AddNote';
-//import MultiFilters from '../components/MultiFilters';
 
- 
 const Reflection = () => {
-    const [notes, setNotes] = useState([
-        {
-            id: nanoid(),
-            title: "title",
-            text: "third Note",
-            date: "22/11/23",
-            Mood: 'happy'
-        }
-    ]);
- 
+    const [notes, setNotes] = useState([]);
     const [searchText, setSearchText] = useState('');
     const [searchTitle, setSearchTitle] = useState('');
- 
-    const addNote = (text, title, mood) => {
-        const date = new Date();
-        const newNote = {
-            id: nanoid(),
-            title: title,
-            text: text,
-            date: date.toLocaleDateString(),
-            mood: mood,
+
+    useEffect(() => {
+        // Fetch reflections from the backend on component mount
+        fetchReflections();
+    }, []);
+
+    const fetchReflections = async () => {
+        try {
+            const response = await fetch('http://localhost:8080/api/reflections/all');
+            if (response.ok) {
+                const data = await response.json();
+                setNotes(data);
+            } else {
+                console.error('Failed to fetch reflections:', response.statusText);
+            }
+        } catch (error) {
+            console.error('An error occurred while fetching reflections:', error);
         }
-        const newNotes = [...notes, newNote];
-        setNotes(newNotes);
     };
- 
-    const deleteNote = (id) => {
-const newNotes = notes.filter((note) => note.id !== id);
-        setNotes(newNotes);
-    }
- 
+
+    const addNote = async (text, title, mood) => {
+        try {
+          const date = new Date().toLocaleDateString();
+          const response = await fetch('http://localhost:8080/api/reflections/add', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ text, title, mood, date }),
+          });
+      
+          if (response.ok) {
+            
+            const newNote = await response.json();
+            setNotes((prevNotes) => [...prevNotes, newNote]);
+          } else {
+            console.error('Failed to add reflection:', response.statusText);
+          }
+        } catch (error) {
+          console.error('An error occurred while adding reflection:', error);
+        }
+      };
+      
+      const deleteNote = async (id) => {
+        try {
+          const response = await fetch(`http://localhost:8080/api/reflections/delete/${id}`, {
+            method: 'DELETE',
+          });
+      
+          if (response.ok) {
+            setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
+          } else {
+            console.error('Failed to delete reflection:', response.statusText);
+          }
+        } catch (error) {
+          console.error('An error occurred while deleting reflection:', error);
+        }
+      };
+
     return (
         <div>
             <div className='header'>
-                <h1>Reflection</h1>
-                {/* <MultiFilters/> */}
+                <h1 className='header'>Reflection</h1>
             </div>
             <div className='cons'>
-            <Search handleSearchNote={setSearchText} handleSearchTitle={setSearchTitle} />
+                <Search handleSearchNote={setSearchText} handleSearchTitle={setSearchTitle} />
                 <NotesList
+                
                     notes={notes.filter((note) =>
                         (note.text?.toLowerCase().includes(searchText) || false) ||
                         (note.title?.toLowerCase().includes(searchTitle) || false)
@@ -60,5 +86,5 @@ const newNotes = notes.filter((note) => note.id !== id);
         </div>
     );
 };
- 
+
 export default Reflection;
