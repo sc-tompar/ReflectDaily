afe79c8acca46b3ad4dd7360a497570e8b91165c
JerryDoriquez <143570720+JerryDoriquez@users.noreply.github.com>
2023-12-06 16:02:44 +0800

    Update NotesList.jsx

--- components/NotesList.jsx
+++ components/NotesList.jsx
@@ -1,26 +1,25 @@
-// NotesList.jsx
-import React from 'react';
-import Note from '../components/Note';
-import AddNote from '../components/AddNote';
-import '../styles/Reflection.css';
+import Note from './Note';
+import AddNote from './AddNote';
+import '../style/Reflection.css';
 
-const NotesList = ({ notes, handleAddNote, handleDeleteNote }) => {
+const NotesList = ({ notes, handleAddNote, handleDeleteNote, updateNote }) => {
     return (
-      <div className='notes-list'>
-        {notes.map((note) => (
-          <Note
-            key={note.id}
-            id={note.id}
-            title={note.title}
-            text={note.text}
-            date={note.date}
-            mood={note.mood}
-            handleDeleteNote={() => handleDeleteNote(note.id)}
-          />
-        ))}
-        <AddNote handleAddNote={handleAddNote} />
-      </div>
+        <div className='notes-list'>
+            {notes.map((note) => (
+                <Note 
+                    key={note.id}
+                    id={note.id} 
+                    title= {note.title}
+                    text={note.text} 
+                    date={note.date}
+                    mood={note.mood}
+                    handleDeleteNote={handleDeleteNote}
+                    updateNote={updateNote}
+                    />
+            ))}
+            <AddNote handleAddNote={handleAddNote}/>
+        </div>
     );
-  };
+};
 
 export default NotesList;
