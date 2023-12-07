c75a775e3f6e974ecc85bb837d3ce7a5dc8f6e4d
Wrecage <121277075+Wrecage@users.noreply.github.com>
2023-12-07 22:30:01 +0800

    Update NotesList.jsx (passing of updateNote)

--- components/NotesList.jsx
+++ components/NotesList.jsx
@@ -1,23 +1,26 @@
-import Note from './Note';
-import AddNote from './AddNote';
-import '../style/Reflection.css';
+import React from 'react';
+import Note from '../components/Note';
+import AddNote from '../components/AddNote';
+import '../styles/Reflection.css';
 
 const NotesList = ({ notes, handleAddNote, handleDeleteNote, updateNote }) => {
     return (
         <div className='notes-list'>
             {notes.map((note) => (
-                <Note 
+                <Note
                     key={note.id}
-                    id={note.id} 
-                    title= {note.title}
-                    text={note.text} 
+                    id={note.id}
+                    title={note.title}
+                    text={note.text}
                     date={note.date}
                     mood={note.mood}
-                    handleDeleteNote={handleDeleteNote}
-                    updateNote={updateNote}
-                    />
+                    handleDeleteNote={() => handleDeleteNote(note.id)}
+                    updateNote={(id, editedTitle, editedText, editedMood) =>
+                        updateNote(note.id, editedTitle, editedText, editedMood)
+                    }
+                />
             ))}
-            <AddNote handleAddNote={handleAddNote}/>
+            <AddNote handleAddNote={handleAddNote} />
         </div>
     );
 };
