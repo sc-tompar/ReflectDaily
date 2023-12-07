5cff5aa2d5a885ea60c9fc3f617fefe4c2ba1399
Wrecage <121277075+Wrecage@users.noreply.github.com>
2023-12-07 22:29:27 +0800

    Update Note.jsx (with Editable api, Delete api, Image still conflict {neglect lang sa} )

--- components/Note.jsx
+++ components/Note.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { MdDeleteForever, MdEdit, MdSave, MdInsertPhoto } from 'react-icons/md';
-import '../style/Reflection.css';
+import '../styles/Reflection.css';
 
-const Note = ({ id, title, text, date, mood, handleDeleteNote, updateNote }) => {
+const Note = ({id, title, text, date, mood, handleDeleteNote, updateNote }) => {
     const [isEditing, setIsEditing] = useState(false);
     const [editedTitle, setEditedTitle] = useState(title);
     const [editedText, setEditedText] = useState(text);
@@ -12,32 +12,88 @@ const Note = ({ id, title, text, date, mood, handleDeleteNote, updateNote }) => 
         setIsEditing(true);
     };
 
-    const handleSaveClick = () => {
-        updateNote(id, editedTitle, editedText, editedMood);
-        setIsEditing(false);
+    
+    const handleSaveClick = async () => {
+      try {
+          const response = await fetch(`http://localhost:8080/api/reflections/update/${id}`, {
+              method: 'PUT',
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                  title: editedTitle,
+                  text: editedText,
+                  mood: editedMood,
+              }),
+          });
+
+          if (response.ok) {
+              updateNote(id, editedTitle, editedText, editedMood); // Pass the correct parameters
+              alert('Record successfully edited'); // Show success message
+              setIsEditing(false);
+          } else {
+              console.error('Failed to edit reflection:', response.statusText);
+              alert('Failed to edit record'); // Show error message
+          }
+      } catch (error) {
+          console.error('An error occurred while editing reflection:', error);
+          alert('An error occurred while editing record'); // Show error message
+      }
+  };
+
+    const handleDeleteClick = async () => {
+        const confirmed = window.confirm('Are you sure you want to delete this record?');
+        if (confirmed) {
+            try {
+                const response = await fetch(`http://localhost:8080/api/reflections/delete/${id}`, {
+                    method: 'DELETE',
+                });
+
+                if (response.ok) {
+                    handleDeleteNote(); // Fetch updated reflections after deleting one
+                    alert('Record successfully deleted'); // Show success message
+                } else {
+                    console.error('Failed to delete reflection:', response.statusText);
+                    alert('Failed to delete record'); // Show error message
+                }
+            } catch (error) {
+                console.error('An error occurred while deleting reflection:', error);
+                alert('An error occurred while deleting record'); // Show error message
+            }
+        }
     };
 
     const handleInsertImage = () => {
-        // Create a file input element
-        const input = document.createElement('input');
-        input.type = 'file';
-
-        // Trigger the file input click event
-        input.click();
-
-        // Handle the file selection event
-        input.addEventListener('change', (event) => {
-            const file = event.target.files[0];
-
-            if (file) {
-                // You may want to use FileReader or upload the file to a server
-                // For simplicity, I'll set the image URL directly
-                const imageUrl = URL.createObjectURL(file);
-                setEditedText((prevText) => `${prevText} ![Alt Text](${imageUrl}) `);
+      const input = document.createElement('input');
+      input.type = 'file';
+      input.click();
+      input.addEventListener('change', async (event) => {
+        const file = event.target.files[0];
+        if (file) {
+          try {
+            const formData = new FormData();
+            formData.append('image', file);
+    
+            const response = await fetch(`http://localhost:8080/api/reflections/upload-image`, {
+              method: 'POST',
+              body: formData,
+            });
+            //I NEGLECT LANG SA NI NING handleInsertImage 
+    
+            if (response.ok) {
+              const imageUrl = await response.text(); // Use text() instead of json()
+              setEditedText((prevText) => `${prevText} ![Alt Text](${imageUrl}) `);
+            } else {
+              console.error('Failed to upload image:', response.statusText);
             }
-        });
+          } catch (error) {
+            console.error('An error occurred while uploading image:', error);
+          }
+        }
+      });
     };
-
+    
+    
     return (
         <div className={`note ${isEditing ? 'editing' : ''}`}>
             {isEditing ? (
@@ -62,6 +118,9 @@ const Note = ({ id, title, text, date, mood, handleDeleteNote, updateNote }) => 
                     <button onClick={handleSaveClick}>
                         <MdSave className='save-icon' size='1.3em' />
                     </button>
+                    <button onClick={() => setIsEditing(false)}>
+                        Cancel
+                    </button>
                     <button onClick={handleInsertImage}>
                         <MdInsertPhoto className='insert-image-icon' size='1.3em' />
                     </button>
@@ -73,10 +132,13 @@ const Note = ({ id, title, text, date, mood, handleDeleteNote, updateNote }) => 
                         <div className='note-icons'>
                             <MdEdit onClick={handleEditClick} className='edit-icon' size='1.3em' />
                             <MdInsertPhoto className='insert-image-icon' size='1.3em' onClick={handleInsertImage} />
-                            <MdDeleteForever onClick={() => handleDeleteNote(id)} className='delete-icon' size='1.3em' />
+                            <MdDeleteForever onClick={handleDeleteClick} className='delete-icon' size='1.3em' />
                         </div>
                     </div>
+                    
+                    
                     <span className='note-body'>{text}</span>
+                    
                     <div className='note-footer'>
                         <small className='mood'>{mood}</small>
                         <small>{date}</small>
