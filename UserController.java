d3e7e2c429d5a68d76fa7411a8fccf3a548f9faf
stevelaurenz10 <146915776+stevelaurenz10@users.noreply.github.com>
2023-12-14 16:51:23 +0800

    Add files via upload

--- UserController.java
+++ UserController.java
@@ -4,34 +4,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+
 import com.csit321.Espanot.LoginRequest;
 import com.csit321.Espanot.Entity.UserEntity;
 import com.csit321.Espanot.Service.UserService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
-    public UserEntity registerUser(@RequestBody UserEntity user) {
-        return userService.registerUser(user);
+    public ResponseEntity<?> registerUser(@RequestBody UserEntity user) {
+        return userService.registerUser(user); 
     }
     
     @PostMapping("/login")
-    public ResponseEntity<String> loginUser(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest) {
         try {
             UserEntity user = userService.getUserByEmailAndPassword(loginRequest.getEmail(), loginRequest.getPassword());
-            return ResponseEntity.ok("Login successful for user: " + user.getEmail());
+            // Create a Map or a DTO to send necessary user information
+            Map<String, Object> response = new HashMap<>();
+            response.put("firstName", user.getFirstName());
+            response.put("lastName", user.getLastName());
+            response.put("email", user.getEmail());
+            // Add more fields as necessary but DO NOT include sensitive data like password
+            return ResponseEntity.ok(response);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Invalid email or password");
         }
     }
-  
+
+ 
 
     @GetMapping("/getAllUsers")
     public List<UserEntity> getAllUsers() {
@@ -47,7 +59,6 @@ public class UserController {
     public String deleteUser(@PathVariable Long userId) {
         return userService.deleteUser(userId);
     }
-    
     
     
     
